1288d1c4b85c6080dc9e7072fdae5dfc6afb0668
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-20 02:56:06 +0000

    Some debug messages for Heroku

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,12 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
+    console.log("we are here");
     knex
       .select("*")
       .from("users")
       .then((results) => {
+        console.log("we are at the then", results);
         res.json(results);
     });
   });
