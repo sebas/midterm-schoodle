3fd77a663445256c1c29d8d3cacb6f0dd9d7fa82
Spencer Shupe <shupe@protonmail.com>
2018-09-21 18:58:58 -0700

    ajax for voting and retrieving poll options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,32 @@
 $(() => {
+  var event_id = $('section').data("event_id")
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+    url: `/api/events/pollOptions/${event_id}`
+  }).done((eventOptionsArray) => {
+    console.log(eventOptionsArray)
+    eventOptionsArray.forEach( eventOption => {
+      $(`<input type="radio" id="${eventOption.id}" name="event_option" value="${eventOption.id}">${eventOption.option_text}<br>`).prependTo($("#voteForm"));
+    }) 
+  });
+  $("#voteButton").on("click",function(event){
+    event.preventDefault();
+    var username = $("#organizer_name").html();
+    var email = $("#organizer_email").html();
+    var event_option_id = $('input[name="event_option"]:checked').val();
+    var super_secret_URL = $('header').data("super_secret_url");
+    console.log("event_option_id",event_option_id);
+    $.ajax({
+      method: "POST",
+      url: "/vote",
+      data: {
+        username,
+        email,
+        event_id,
+        event_option_id,
+        super_secret_URL
+      }
+    })
+  })
+
 });
