88b243cddf43b47d5b256445d4d21285b82a2eeb
Spencer Shupe <shupe@protonmail.com>
2018-09-22 15:06:33 -0700

    query db for participants with event_option_id and draw each vote

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,35 @@
-$(() => {
+$(function() {
   var event_id = $('section').data("event_id")
+
   $.ajax({
     method: "GET",
     url: `/api/events/pollOptions/${event_id}`
-  }).done((eventOptionsArray) => {
-    console.log(eventOptionsArray)
-    eventOptionsArray.forEach( eventOption => {
-      $(`<input type="radio" id="${eventOption.id}" name="event_option" value="${eventOption.id}">${eventOption.option_text}<br>`).prependTo($("#voteForm"));
-    }) 
+  }).then(function(eventOptionsArray) {
+    eventOptionsArray.forEach( function(eventOption) {
+      $(`<div id="${eventOption.id}-list">Voters:</div>`).prependTo("section")
+      $(`<input type="radio" id="${eventOption.id}" name="event_option" value="${eventOption.id}">${eventOption.option_text}<br>`).prependTo($(`#${eventOption.id}-list`));
+    })
+    return eventOptionsArray;
+   }).then(function(eventOptionsArray) {
+    eventOptionsArray.forEach(function(option){
+      $.ajax({
+        method: "GET",
+        url: `/api/events/${option.id}/participants/`
+      }).done(function(participantsArray) {
+        participantsArray.forEach( function(participant) {
+          $(`<div id="${participant.email}"> ${participant.username} (${participant.email})</div>`).appendTo($(`#${participant.event_option_id}-list`));
+        });
+      });
+    });
   });
-  $("#voteButton").on("click",function(event){
+
+  $("#voteButton").on("click", function(event){
     event.preventDefault();
-    var username = $("#organizer_name").html();
-    var email = $("#organizer_email").html();
-    var event_option_id = $('input[name="event_option"]:checked').val();
+    var username = $("#organizer_name").val();
+    var email = $("#organizer_email").val();
+    var option = $('input[name="event_option"]:checked').val();
     var super_secret_URL = $('header').data("super_secret_url");
-    console.log("event_option_id",event_option_id);
+    console.log("option",option);
     $.ajax({
       method: "POST",
       url: "/vote",
@@ -23,10 +37,13 @@ $(() => {
         username,
         email,
         event_id,
-        event_option_id,
+        option,
         super_secret_URL
-      }
-    })
+    },
+      success: function(response){
+        window.location.href = response;
+      } 
+    });
   })
 
 });
